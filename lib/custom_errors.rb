6cda32db17c79800fbec78cf33dbaf00c226334b
James Parrish <jthomasparrishjr@gmail.com>
2022-11-10 18:17:48 -0600

    commit tree 41b157c38c61f4170d239cd43b4c910c4c6e38bc

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,8 +6,22 @@ class Person
   end
 
   def get_married(person)
+    if person.is_a?(Person)
     self.partner = person
     person.partner = self
+  else
+    begin
+    raise PartnerError
+    rescue PartnerError => error 
+      puts error.message
+  end
+end
+end
+
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
   end
 
 end
@@ -15,6 +29,7 @@ end
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
+
 
 
 
